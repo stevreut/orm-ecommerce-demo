897d483ad9346f4b709ea9131585610132786cc3
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-08-30 07:33:08 -0400

    all /api/categories get routes tested (initially)

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,22 +2,35 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
+
 router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const CategoryData = await Category.findAll({
+    const categoryData = await Category.findAll({
       include: [{ model: Product }],
     });
-    res.status(200).json(CategoryData);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category with this id found!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
