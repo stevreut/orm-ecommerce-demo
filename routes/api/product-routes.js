d4f345af2f61209d808f5cb508f5abb6befeb0bf
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-08-30 19:43:08 -0400

    get product all working, including related categories and tags

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,50 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  console.log('get prod all');
+  try {
+    const productData = await Product.findAll({
+      include: [
+        { 
+          model: Category,
+          required: false 
+        },
+        {
+          model: Tag,
+          required: false
+        }
+      ],
+    });
+    console.log('get prod productData = ', productData);
+    // TODO - *Tag* data is still to-do
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{
+         model: Category, 
+         required: false  // enables LEFT JOIN, thereby showing products without categories
+        }]
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product with this id found!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
