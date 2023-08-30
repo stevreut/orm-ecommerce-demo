21014fa25f5f842c3ba24999e46bee1fd87c1c7e
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-08-29 23:38:07 -0400

    add many-to-many linkages between Tag and Product (refs still needed in models)

--- models/index.js
+++ models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+  // TODO : onDelete - or let default apply?
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag
+  // TODO : onDelete - or let default apply?
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag
+  // TODO : onDelete - or let default apply?
+})
 
 module.exports = {
   Product,
