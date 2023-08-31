d8e16514c1cd303230a985d31efd2e163bc68221
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-08-30 20:08:28 -0400

    get products by ID working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -35,10 +35,16 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{
+      include: [
+        {
          model: Category, 
          required: false  // enables LEFT JOIN, thereby showing products without categories
-        }]
+        },
+        {
+          model: Tag,
+          required: false  // TODO - LEFT JOIN, ...
+        }
+      ]
     });
     if (!productData) {
       res.status(404).json({ message: 'No product with this id found!' });
