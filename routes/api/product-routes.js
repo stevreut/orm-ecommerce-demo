bb45982c70d33ddc3c3dd9c28392eb1e2713144b
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-08-31 20:13:53 -0400

    product-routes.js finalized functionally (for real this time)

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -142,8 +142,27 @@ router.put('/:id', async (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  console.log('del prod this.id = ', this.id);
+  console.log('del prod req.params = ', req.params);
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    console.log('del productData = ', productData);
+    if (!productData) {
+      console.log('del prod when falsy', productData);
+      res.status(404).json({ message: 'No product with specified ID was found to be deleted!' });
+    } else {
+      res.status(200).json(productData);
+      console.log('del prod when truthy ', productData);
+    }
+  } catch (err) {
+    res.status(500).json(err);  // TODO - 500?
+  }
 });
 
 module.exports = router;
