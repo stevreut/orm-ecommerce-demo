443317a71e506cd9bc5937f2fc5f90dd8e2f561a
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-08-31 21:18:23 -0400

    tag gets and add now working, insomnia updated backup

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -22,13 +22,18 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  console.log('tag get entry point');
   try {
+    console.log('tag get try');
+    console.log('tag get - req.params = ', req.params);
     const tagData = await Tag.findByPk(req.params.id, {
       include: [{
          model: Product, 
          required: false  // enables LEFT JOIN, thereby showing tags without associated products
         }]
     });
+    console.log('tag get complete');
+    console.log('tag data = ', tagData);
     if (!tagData) {
       res.status(404).json({ message: 'No tag with this id found!' });
       return;
@@ -41,6 +46,33 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  /* req.body should look like this...
+    {
+      tag_name: "aquamarine",
+      productIds: [1, 2, 3, 4]
+    }
+  */
+    Tag.create(req.body)
+    .then((tag) => {
+      // if there's tag tags, we need to create pairings to bulk create in the ProductTag model
+      if (req.body.productIds.length) {
+        const productTagIdArr = req.body.productIds.map((prod_id) => {
+          return {
+            product_id: prod_id,
+            tag_id: tag.id
+          };
+        });
+        return ProductTag.bulkCreate(productTagIdArr);
+      }
+      // if no product tags, just respond
+      res.status(200).json(tag);
+    })
+    .then((productTagIds) => res.status(200).json(productTagIds))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+
 });
 
 router.put('/:id', (req, res) => {
